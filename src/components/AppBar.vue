055d6a2a4e17d9e4edd6e30feb41b6e22a709152
JegoRamos <jego.ramos@blackpearl.technology>
2019-12-03 16:37:40 +0800

    initial commit

---
+++ src/components/AppBar.vue
@@ -0,0 +1,111 @@
+<template>
+	<nav>
+		<v-app-bar app>
+			<v-toolbar-title class="headline text-uppercase">
+				<router-link :to="{name: 'home'}" class="title">
+					<v-toolbar-title class="text-uppercase">
+						<!-- Material icons are found here https://cdn.materialdesignicons.com/3.6.95/ -->
+						<v-icon class="indigo--text mr-1 mb-1">mdi-school</v-icon>
+						<span class="font-weight-bold indigo--text">Workshop</span>
+						<span class="font-weight-light indigo--text">Day1</span>
+					</v-toolbar-title>
+				</router-link>
+			</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<!-- Appbar links. Will be hidden on mobile view -->
+			<div class="hidden-sm-and-down">
+				<!-- Links -->
+				<v-btn text v-for="link in links" :key="link.name" :href="link.address" target="_blank">
+					<v-icon small class="mr-1 indigo--text">{{ link.icon }}</v-icon>
+					<span class="mr-2 indigo--text">{{ link.name }}</span>
+				</v-btn>
+			</div>
+			<v-app-bar-nav-icon @click="drawer = !drawer" class="hidden-md-and-up indigo--text"></v-app-bar-nav-icon>
+		</v-app-bar>
+
+		<!-- Nav drawer. This appears on mobile view -->
+		<v-navigation-drawer app v-model="drawer" temporary right>
+			<!-- Account link - Ignore, for aesthetics only -->
+			<v-list-item class="my-2">
+				<avatar :username="sampleUser.name" class="mr-5"></avatar>
+				<v-list-item-content>
+					<v-list-item-title>{{ sampleUser.name }}</v-list-item-title>
+					<v-list-item-subtitle>{{ sampleUser.bio }}</v-list-item-subtitle>
+				</v-list-item-content>
+			</v-list-item>
+			<v-divider></v-divider>
+			<v-list dense>
+				<!-- Links -->
+				<v-list-item
+					v-for="link in links"
+					:key="link.name"
+					:href="link.address"
+					target="_blank"
+					exact
+					link
+					active-class="indigo--text"
+				>
+					<v-list-item-icon>
+						<v-icon>{{ link.icon }}</v-icon>
+					</v-list-item-icon>
+					<v-list-item-content>
+						<v-list-item-title>{{ link.name }}</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+	</nav>
+</template>
+
+<script>
+	import Avatar from 'vue-avatar'
+	export default {
+		components: {
+			avatar: Avatar
+		},
+		data() {
+			return {
+				drawer: false,
+				links: [
+					{
+						name: 'Google Maps',
+						address: 'https://www.google.com/maps',
+						icon: 'mdi-google-maps'
+					},
+					{
+						name: 'Google Play',
+						address: 'https://play.google.com',
+						icon: 'mdi-google-play'
+					},
+					{
+						name: 'Chrome',
+						address: 'https://www.google.com/chrome',
+						icon: 'mdi-google-chrome'
+					},
+					{
+						name: 'Youtube',
+						address: 'https://youtube.com',
+						icon: 'mdi-youtube'
+					},
+					{ name: 'Gmail', address: 'https://gmail.com', icon: 'mdi-gmail' }
+				],
+				sampleUser: {
+					name: 'Ned Stark',
+					bio: 'Die in Season 1'
+				}
+			}
+		},
+		methods: {},
+		computed: {},
+		filters: {}
+	}
+</script>
+
+
+<style scoped>
+	.title {
+		text-decoration: none;
+		text-decoration-style: none;
+		color: white;
+	}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-container>
+		<v-row justify="center">
+			<v-col cols="12" sm="10">
+				<v-card class="px-3 pb-3">
+					<v-card-title class="justify-center grey--text">BEST APP IN THE WORLD</v-card-title>
+					<v-divider class="mb-5" />
+					<div class="d-flex justify-center align-center ma-5">
+						<v-btn @click="dec()" fab small elevation="0" class="mr-5 blue" dark>
+							<v-icon>mdi-minus</v-icon>
+						</v-btn>
+						<span class="display-4 mx-5 font-weight-thin grey--text">{{ counter }}</span>
+						<v-btn @click="inc()" fab small elevation="0" class="ml-5 red" dark>
+							<v-icon>mdi-plus</v-icon>
+						</v-btn>
+					</div>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		data() {
+			return {
+				counter: 0
+			}
+		},
+		methods: {
+			inc() {
+				this.counter += 1
+			},
+			dec() {
+				if (this.counter > 0) {
+					this.counter -= 1
+				}
+			}
+		}
+	}
 </script>
+
+<style>
+</style>
