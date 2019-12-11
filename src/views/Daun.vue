0e982bf79c3b12ef0f76fa18abc6da0e44502163
DaunSayson <daun.sayson@blackpearl.technology>
2019-12-11 16:32:30 +0800

    before merge with feature/si-sprint2

--- src/views/Daun.vue
+++ src/views/Daun.vue
@@ -6,7 +6,14 @@
 				<v-card class="px-3 pb-3">
 					<v-card-title class="justify-center grey--text">DAUN SAYSON</v-card-title>
 					<v-divider class="mb-5" />
-					<div>
+					<div class="text-right">
+                            <v-text-field label="Agency Name" outlined></v-text-field>
+                            <v-text-field label="Agency Type" outlined></v-text-field>
+                            <v-text-field label="Agency Number" outlined></v-text-field>
+                            <v-text-field label="Agency Address" outlined></v-text-field>
+                            <v-btn class="ma-2" outlined fab color="indigo">
+                            <v-icon>mdi-plus</v-icon>
+                            </v-btn>
 					</div>
 				</v-card>
 			</v-col>
