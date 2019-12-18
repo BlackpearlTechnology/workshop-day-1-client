4eddc328dd0e99a77328708a678dd325334f6e9a
JegoRamos <jego.ramos@blackpearl.technology>
2019-12-18 11:21:25 +0800

    added unit tests

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,65 @@
 <template>
-	<v-container>
-		<v-row justify="center">
-			<v-col cols="12" sm="10">
-				<v-card class="px-3 pb-3">
-					<v-card-title class="justify-center grey--text">BEST APP IN THE WORLD</v-card-title>
-					<v-divider class="mb-5" />
-					<div class="d-flex justify-center align-center ma-5">
-						<v-btn @click="dec()" fab small elevation="0" class="mr-5 blue" dark>
-							<v-icon>mdi-minus</v-icon>
-						</v-btn>
-						<span class="display-4 mx-5 font-weight-thin grey--text">{{ counter }}</span>
-						<v-btn @click="inc()" fab small elevation="0" class="ml-5 red" dark>
-							<v-icon>mdi-plus</v-icon>
-						</v-btn>
-					</div>
-				</v-card>
-			</v-col>
-		</v-row>
-	</v-container>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="10">
+        <v-card class="px-3 pb-3">
+          <v-card-title class="justify-center grey--text"
+            >BEST APP IN THE WORLD</v-card-title
+          >
+          <v-divider class="mb-5" />
+          <div class="d-flex justify-center align-center ma-5">
+            <v-btn
+              id="dec-btn"
+              @click="dec()"
+              fab
+              small
+              elevation="0"
+              class="mr-5 blue"
+              dark
+            >
+              <v-icon>mdi-minus</v-icon>
+            </v-btn>
+            <span
+              id="result-text"
+              class="display-4 mx-5 font-weight-thin grey--text"
+              >{{ counter }}</span
+            >
+            <v-btn
+              id="inc-btn"
+              @click="inc()"
+              fab
+              small
+              elevation="0"
+              class="ml-5 red"
+              dark
+            >
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				counter: 0
-			}
-		},
-		methods: {
-			inc() {
-				this.counter += 1
-			},
-			dec() {
-				if (this.counter > 0) {
-					this.counter -= 1
-				}
-			}
-		}
-	}
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    inc() {
+      this.counter += 1
+    },
+    dec() {
+      if (this.counter > 0) {
+        this.counter -= 1
+      }
+    }
+  }
+}
 </script>
 
-<style>
-</style>
+<style></style>
